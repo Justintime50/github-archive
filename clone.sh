becff2653b73626b79cad5a8f4542cabe3b49c0e
Justintime50 <justinpaulhammond@gmail.com>
2019-12-19 11:01:36 -0700

    Fixed directory to clone into

--- clone.sh
+++ clone.sh
@@ -1,3 +1,5 @@
+mkdir -p $HOME/github-archive
+cd $HOME/github-archive
 CNTX={users}; NAME={Justintime50}; PAGE=1
 curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
   grep -e 'git_url*' |
