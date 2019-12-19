aa4e4eee738c1be90da34a4d8dbfc14cd3b4b316
Justintime50 <justinpaulhammond@gmail.com>
2019-12-19 12:17:58 -0700

    Updated script to pull repos too

--- clone.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p $HOME/github-archive
-cd $HOME/github-archive
-CNTX={users}; NAME={Justintime50}; PAGE=1
-curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
-  grep -e 'git_url*' |
-  cut -d \" -f 4 |
-  xargs -L1 git clone 
