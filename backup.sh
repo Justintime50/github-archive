279b96281000294186f04afa2c8684259901e602
Justintime50 <justinpaulhammond@gmail.com>
2020-01-15 01:08:14 -0700

    Works with private repos, orgs, and, clones to dir

--- backup.sh
+++ backup.sh
@@ -3,32 +3,50 @@
 # Import config and check if something is missing
 # shellcheck disable=SC1091
 source .config
-if [ -z "$SCOPE" ] || [ -z "$USERNAME" ] || [ -z "$TOKEN" ] || [ -z "$PAGE" ] || [ -z "$DATE" ] || [ -z "$LOG_LIFE" ] || [ -z "$LOCATION" ] ; then
+if [ -z "$USERNAME" ] || [ -z "$TOKEN" ] || [ -z "$PAGE" ] || [ -z "$DATE" ] || [ -z "$LOG_LIFE" ] || [ -z "$LOCATION" ] ; then
     echo "You have variables that have no values, please set them in the \".config\" before continuing."
     exit
 fi
 
 # Setup required directories
-mkdir -p "$LOCATION"
-mkdir -p "$LOCATION"/archive-logs
-cd "$LOCATION" || exit
+mkdir -p "$LOCATION"/repos
+mkdir -p "$LOCATION"/logs
+cd "$LOCATION"/repos || exit
 
-# Clone all repos you have access to
+# Start archive
 {
-curl -u "$USERNAME":"$TOKEN" "https://api.github.com/$SCOPE/$USERNAME/repos?page=$PAGE&per_page=100" |
-        grep -e 'git_ur[l]*' |
-        cut -d \" -f 4 |
-        xargs -L1 git clone
+    # Clone all repos from the user's account
+    mkdir -p "$LOCATION"/repos/"$USERNAME"
+    cd "$LOCATION"/repos/"$USERNAME" || exit
+    curl -u "$USERNAME":"$TOKEN" -s "https://api.github.com/users/$USERNAME/repos?page=$PAGE&per_page=100" |
+    python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["ssh_url"])'
+    cd .. || exit
 
-# Pull any changes
-for DIR in */ ; do
-    printf '%s\n' "$DIR"
-    cd "$DIR" && git pull origin master
-    cd ..
-done
-echo "Github Archive Complete!"
-} 2>&1 | tee "$LOCATION"/archive-logs/"$DATE".log
+    # Clone all repos from the configured orgs you have access to
+    for ORG in ${ORGS[*]} ; do
+        mkdir -p "$LOCATION"/repos/"$ORG"
+        cd "$LOCATION"/repos/"$ORG" || exit
+        curl -u "$USERNAME":"$TOKEN" -s "https://api.github.com/orgs/$ORG/repos?page=$PAGE&per_page=100" |
+        python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["ssh_url"])'
+        cd .. || exit
+    done
+
+    # Pull any changes from each directory in the archive
+    DIRARRAY="$USERNAME $ORGS"
+    for DIRARRAY in */ ; do
+        printf '%s\n' "$DIRARRAY"
+        cd "$DIRARRAY" || exit
+        for DIR in */ ; do
+            printf '%s\n' "$DIR"
+            cd "$DIR" && git pull origin master
+            cd .. || exit
+        done
+        cd .. || exit
+    done
+
+    echo "Github Archive Complete!"
+} 2>&1 | tee "$LOCATION"/logs/"$DATE".log
 
 # Cleanup after being run
-find "$LOCATION"/archive-logs -mindepth 1 -mtime +"$LOG_LIFE" -delete
+find "$LOCATION"/logs -mindepth 1 -mtime +"$LOG_LIFE" -delete
 history -c
