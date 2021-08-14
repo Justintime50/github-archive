e271e58524545be2d6cda909e01b26955b921254
Justin Hammond <39606064+Justintime50@users.noreply.github.com>
2021-08-13 23:11:07 -0600

    chore: large refactor of names, format, and logger messages (#27)
    * chore: large refactor of names, format, and logger messages
    
    * fix: clarify git_command
    
    * fix: additional tweaks
    
    * fix: breaking up github loging for future use

--- github_archive/archive.py
+++ github_archive/archive.py
@@ -9,144 +9,143 @@ from github import Github
 
 from github_archive.logger import Logger
 
+# TODO: Add user/password authentication (will need to pull from non-ssh url)
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+GITHUB_LOGIN = Github(GITHUB_TOKEN)
+AUTHENTICATED_GITHUB_USER = GITHUB_LOGIN.get_user()
 ORG_LIST = os.getenv('GITHUB_ARCHIVE_ORGS', '')
 ORGS = ORG_LIST.split(',')
-GITHUB_ARCHIVE_LOCATION = os.path.expanduser(
-    os.getenv('GITHUB_ARCHIVE_LOCATION', '~/github-archive')
-)
-LOGGER = logging.getLogger(__name__)
-USER = Github(GITHUB_TOKEN).get_user()
-# TODO: Add user/password authentication (will need to pull from non-ssh url)
-# BUFFER is the time in between each request - helps with rate limiting
-BUFFER = float(os.getenv('GITHUB_ARCHIVE_BUFFER', 0.1))
-# GIT_TIMEOUT is the number of seconds before a git operation (clone, pull, etc) will timeout
+
 GIT_TIMEOUT = int(os.getenv('GITHUB_ARCHIVE_TIMEOUT', 180))
+GITHUB_ARCHIVE_LOCATION = os.path.expanduser(os.getenv('GITHUB_ARCHIVE_LOCATION', '~/github-archive'))
+SUBPROCESS_BUFFER = float(
+    os.getenv('GITHUB_ARCHIVE_BUFFER', 0.1)
+)  # TODO: This can most likely be removed once we limit the number of open threads at once
+
+LOGGER = logging.getLogger(__name__)
+CLONE_OPERATION = 'clone'
+ORG_CONTEXT = 'orgs'
+PULL_OPERATION = 'pull'
+USER_CONTEXT = 'user'
 
 
-class GithubArchive():
+class GithubArchive:
     @staticmethod
     def run(user_clone=False, user_pull=False, gists_clone=False, gists_pull=False, orgs_clone=False, orgs_pull=False):
-        """Run the tool based on the arguments passed
-        """
-        GithubArchive.initialize_project()
+        """Run the tool based on the arguments passed."""
+        GithubArchive.initialize_project(orgs_clone, orgs_pull)
         Logger._setup_logging(LOGGER)
-        clone = 'clone'
-        pull = 'pull'
         LOGGER.info('# GitHub Archive started...\n')
         start_time = datetime.now()
 
+        # Make API calls
         if user_clone or user_pull:
-            repos = GithubArchive.get_repos()
-
-        if user_clone is True:
-            LOGGER.info('# Cloning personal repos...\n')
-            GithubArchive.determine_repo_context(repos, 'user', clone)
-
-        if user_pull is True:
-            LOGGER.info('# Pulling personal repos...\n')
-            GithubArchive.determine_repo_context(repos, 'user', pull)
-
-        if ORG_LIST != '' and orgs_clone or orgs_pull:
+            LOGGER.info('# Making API call to GitHub for personal repos...\n')
+            repos = GithubArchive.get_personal_repos()
+        if orgs_clone or orgs_pull:
+            LOGGER.info('# Making API calls to GitHub for org repos...\n')
             org_repos = GithubArchive.get_all_org_repos()
-
-            if orgs_clone is True:
-                LOGGER.info('# Cloning org repos...\n')
-                GithubArchive.determine_repo_context(org_repos, 'orgs', clone)
-
-            if orgs_pull is True:
-                LOGGER.info('# Pulling org repos...\n')
-                GithubArchive.determine_repo_context(org_repos, 'orgs', pull)
-
         if gists_clone or gists_pull:
+            LOGGER.info('# Making API call to GitHub for personal gists...\n')
             gists = GithubArchive.get_gists()
 
-        if gists_clone is True:
-            LOGGER.info('# Cloning gists...\n')
-            GithubArchive.iterate_gists(gists, clone)
+        # Git operations
+        if user_clone is True:
+            LOGGER.info('# Cloning missing personal repos...\n')
+            GithubArchive.iterate_repos_to_archive(repos, USER_CONTEXT, CLONE_OPERATION)
+        if user_pull is True:
+            LOGGER.info('# Pulling personal repos...\n')
+            GithubArchive.iterate_repos_to_archive(repos, USER_CONTEXT, PULL_OPERATION)
 
+        if orgs_clone is True:
+            LOGGER.info('# Cloning missing org repos...\n')
+            GithubArchive.iterate_repos_to_archive(org_repos, ORG_CONTEXT, CLONE_OPERATION)
+        if orgs_pull is True:
+            LOGGER.info('# Pulling org repos...\n')
+            GithubArchive.iterate_repos_to_archive(org_repos, ORG_CONTEXT, PULL_OPERATION)
+
+        if gists_clone is True:
+            LOGGER.info('# Cloning missing gists...\n')
+            GithubArchive.iterate_gists_to_archive(gists, CLONE_OPERATION)
         if gists_pull is True:
             LOGGER.info('# Pulling gists...\n')
-            GithubArchive.iterate_gists(gists, pull)
+            GithubArchive.iterate_gists_to_archive(gists, PULL_OPERATION)
 
         execution_time = f'Execution time: {datetime.now() - start_time}.'
         finish_message = f'GitHub Archive complete! {execution_time}\n'
         LOGGER.info(finish_message)
 
     @staticmethod
-    def initialize_project():
-        """Initialize the tool and ensure everything is
-        in order before running any logic
-        """
+    def initialize_project(orgs_clone, orgs_pull):
+        """Initialize the tool and ensure everything is in order before running any logic."""
         if not GITHUB_TOKEN:
             message = 'GITHUB_TOKEN must be present to run github-archive.'
             LOGGER.critical(message)
             raise ValueError(message)
+
+        if not ORG_LIST and (orgs_clone or orgs_pull):
+            message = 'GITHUB_ARCHIVE_ORGS must be present when passing org flags.'
+            LOGGER.critical(message)
+            raise ValueError(message)
+
         if not os.path.exists(GITHUB_ARCHIVE_LOCATION):
             os.makedirs(os.path.join(GITHUB_ARCHIVE_LOCATION, 'repos'))
+
         if not os.path.exists(GITHUB_ARCHIVE_LOCATION):
             os.makedirs(os.path.join(GITHUB_ARCHIVE_LOCATION, 'gists'))
 
     @staticmethod
-    def get_repos():
-        """Retrieve all repos of the authenticated user
-        """
-        repos = USER.get_repos()
+    def get_personal_repos():
+        """Retrieve all repos of the authenticated user."""
+        repos = AUTHENTICATED_GITHUB_USER.get_repos()
+        LOGGER.debug('Personal repos retrieved!')
+
         return repos
 
     @staticmethod
     def get_all_org_repos():
-        """Retrieve repos of all orgs in the orgs list provided
+        """Retrieve repos of all orgs in the orgs list provided and return a single list
+        including every repo from each org flattened.
         """
         all_org_repos = []
         for org in ORGS:
-            all_org_repos.append(Github(GITHUB_TOKEN).get_organization(org.strip()).get_repos())
-        return all_org_repos
+            org_repos = Github(GITHUB_TOKEN).get_organization(org.strip()).get_repos()
+            all_org_repos.append(org_repos)
+            LOGGER.debug(f'{org.strip()} repos retrieved!')
+
+        flattened_org_repos_list = [repo for org_repos in all_org_repos for repo in org_repos]
+
+        return flattened_org_repos_list
 
     @staticmethod
     def get_gists():
-        """Retrieve all gists of the authenticated user
-        """
-        gists = USER.get_gists()
+        """Retrieve all gists of the authenticated user."""
+        gists = AUTHENTICATED_GITHUB_USER.get_gists()
+        LOGGER.debug('User gists retrieved!')
+
         return gists
 
     @staticmethod
-    def determine_repo_context(repos, context, operation):
-        """Determine if a repo is from a user or org
-        and route the logic accordingly
-        """
-        if context == 'orgs':
-            for single_org_repos in repos:
-                GithubArchive.iterate_repos(single_org_repos, context, operation)
-        elif context == 'user':
-            GithubArchive.iterate_repos(repos, context, operation)
-        else:
-            message = f'Could not determine what action to take with {context}.'
-            LOGGER.error(message)
-            raise ValueError(message)
-
-    @staticmethod
-    def iterate_repos(repos, context, operation):
-        """Iterate over each repository
-        """
+    def iterate_repos_to_archive(repos, context, operation):
+        """Iterate over each repository and start a thread if it can be archived."""
         thread_list = []
         for repo in repos:
             # We check the owner name here to ensure that we only iterate
             # through the user's personal repos which will exclude orgs
             # that can instead be iterated by passing the "--clone_orgs"
-            # or "--pull_orgs" flags to allow for granular control
-            if repo.owner.name != USER.name and context == 'user':
+            # or "--pull_orgs" flags to allow for more granular control.
+            if repo.owner.name != AUTHENTICATED_GITHUB_USER.name and context == USER_CONTEXT:
                 continue
             else:
-                time.sleep(BUFFER)
-                path = os.path.join(GITHUB_ARCHIVE_LOCATION, 'repos', repo.owner.login, repo.name)
+                time.sleep(SUBPROCESS_BUFFER)
+                repo_path = os.path.join(GITHUB_ARCHIVE_LOCATION, 'repos', repo.owner.login, repo.name)
                 repo_thread = Thread(
                     target=GithubArchive.archive_repo,
                     args=(
                         repo,
-                        path,
+                        repo_path,
                         operation,
-                    )
+                    ),
                 )
                 thread_list.append(repo_thread)
                 repo_thread.start()
@@ -154,20 +153,19 @@ class GithubArchive():
             thread.join()
 
     @staticmethod
-    def iterate_gists(gists, operation):
-        """Iterate over each gist
-        """
+    def iterate_gists_to_archive(gists, operation):
+        """Iterate over each gist and start a thread if it can be archived."""
         thread_list = []
         for gist in gists:
-            time.sleep(BUFFER)
-            path = os.path.join(GITHUB_ARCHIVE_LOCATION, 'gists', gist.id)
+            time.sleep(SUBPROCESS_BUFFER)
+            gist_path = os.path.join(GITHUB_ARCHIVE_LOCATION, 'gists', gist.id)
             gist_thread = Thread(
                 target=GithubArchive.archive_gist,
                 args=(
                     gist,
-                    path,
+                    gist_path,
                     operation,
-                )
+                ),
             )
             thread_list.append(gist_thread)
             gist_thread.start()
@@ -175,30 +173,27 @@ class GithubArchive():
             thread.join()
 
     @staticmethod
-    def archive_repo(repo, path, operation):
-        """Clone and pull repos based on the operation passed
-        """
-        if os.path.exists(path) and operation == 'clone':
-            LOGGER.info(f'Repo: {repo.name} already cloned, skipping clone operation.')
+    def archive_repo(repo, repo_path, operation):
+        """Clone and pull repos based on the operation passed"""
+        if os.path.exists(repo_path) and operation == CLONE_OPERATION:
+            LOGGER.debug(f'Repo: {repo.name} already cloned, skipping clone operation.')
         else:
-            if operation == 'clone':
-                command = f'git clone {repo.ssh_url} {path}'
-            elif operation == 'pull':
-                command = f'cd {path} && git pull --rebase'
-            else:
-                message = f'Could not determine what action to take with {repo.name} based on {operation}.'
-                LOGGER.error(message)
-                raise ValueError(message)
+            commands = {
+                CLONE_OPERATION: f'git clone {repo.ssh_url} {repo_path}',
+                PULL_OPERATION: f'cd {repo_path} && git pull --rebase',
+            }
+            git_command = commands[operation]
 
             try:
                 subprocess.run(
-                    command,
+                    git_command,
                     stdin=subprocess.DEVNULL,
                     stderr=subprocess.DEVNULL,
                     shell=True,
                     check=True,
-                    timeout=int(GIT_TIMEOUT)
+                    timeout=GIT_TIMEOUT,
                 )
+                # TODO: If the response of pulling is `Already up to date.`, skip outputting to the logger
                 LOGGER.info(f'Repo: {repo.name} {operation} success!')
             except subprocess.TimeoutExpired:
                 LOGGER.error(f'Git operation timed out archiving {repo.name}.')
@@ -206,30 +201,27 @@ class GithubArchive():
                 LOGGER.error(f'Failed to {operation} {repo.name}\n{error}')
 
     @staticmethod
-    def archive_gist(gist, path, operation):
-        """Clone and pull gists based on the operation passed
-        """
-        if os.path.exists(path) and operation == 'clone':
-            LOGGER.info(f'Gist: {gist.id} already cloned, skipping clone operation.')
+    def archive_gist(gist, gist_path, operation):
+        """Clone and pull gists based on the operation passed"""
+        if os.path.exists(gist_path) and operation == CLONE_OPERATION:
+            LOGGER.debug(f'Gist: {gist.id} already cloned, skipping clone operation.')
         else:
-            if operation == 'clone':
-                command = f'git clone {gist.html_url} {path}'
-            elif operation == 'pull':
-                command = f'cd {path} && git pull --rebase'
-            else:
-                message = f'Could not determine what action to take with {gist.id} based on {operation}.'
-                LOGGER.error(message)
-                raise ValueError(message)
+            commands = {
+                CLONE_OPERATION: f'git clone {gist.html_url} {gist_path}',
+                PULL_OPERATION: f'cd {gist_path} && git pull --rebase',
+            }
+            git_command = commands[operation]
 
             try:
                 subprocess.run(
-                    command,
+                    git_command,
                     stdin=subprocess.DEVNULL,
                     stderr=subprocess.DEVNULL,
                     shell=True,
                     check=True,
-                    timeout=int(GIT_TIMEOUT)
+                    timeout=GIT_TIMEOUT,
                 )
+                # TODO: If the response of pulling is `Already up to date.`, skip outputting to the logger
                 LOGGER.info(f'Gist: {gist.id} {operation} success!')
             except subprocess.TimeoutExpired:
                 LOGGER.error(f'Git operation timed out archiving {gist.id}.')
--- github_archive/logger.py
+++ github_archive/logger.py
@@ -2,31 +2,26 @@ import logging
 import logging.handlers
 import os
 
-GITHUB_ARCHIVE_LOCATION = os.path.expanduser(
-    os.getenv('GITHUB_ARCHIVE_LOCATION', '~/github-archive')
-)
+GITHUB_ARCHIVE_LOCATION = os.path.expanduser(os.getenv('GITHUB_ARCHIVE_LOCATION', '~/github-archive'))
 LOG_PATH = os.path.join(GITHUB_ARCHIVE_LOCATION, 'logs')
 LOG_FILE = os.path.join(LOG_PATH, 'github-archive.log')
 LOG_MAX_BYTES = int(os.getenv('GITHUB_ARCHIVE_LOG_MAX_BYTES', 200000))
 LOG_BACKUP_COUNT = int(os.getenv('GITHUB_ARCHIVE_LOG_BACKUP_COUNT', 5))
 
 
-class Logger():
+class Logger:
     @staticmethod
     def _setup_logging(logger):
-        """Setup project logging (to console and log file)
-        """
+        """Setup project logging (to console and log file)"""
         if not os.path.exists(LOG_PATH):
             os.makedirs(LOG_PATH)
         logger.setLevel(logging.INFO)
         handler = logging.handlers.RotatingFileHandler(
             LOG_FILE,
             maxBytes=LOG_MAX_BYTES,
-            backupCount=LOG_BACKUP_COUNT
+            backupCount=LOG_BACKUP_COUNT,
         )
-        formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(message)s"
-        )
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
         handler.setFormatter(formatter)
         logger.addHandler(logging.StreamHandler())
         logger.addHandler(handler)
