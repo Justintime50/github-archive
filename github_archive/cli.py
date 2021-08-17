c7f7d8be8813bfb145bbe79eb195e192ba615042
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-08-17 00:13:26 -0600

    feat!: rework CLI args and class variables

--- github_archive/cli.py
+++ github_archive/cli.py
@@ -1,107 +1,116 @@
 import argparse
 
 from github_archive import GithubArchive
-from github_archive.archive import DEFAULT_NUM_THREADS
+from github_archive.constants import DEFAULT_NUM_THREADS, DEFAULT_TIMEOUT, DEFAULT_LOCATION
 
 
-class CLI:
+class GithubArchiveCli:
     def __init__(self):
-        """Setup the CLI arguments"""
         parser = argparse.ArgumentParser(
             description=(
                 'A powerful script to concurrently clone your entire GitHub instance or save it as an archive.'
             )
         )
         parser.add_argument(
-            '-pc',
-            '--personal_clone',
+            '-v',
+            '--view',
             action='store_true',
             required=False,
             default=False,
-            help='Clone personal repos.',
+            help='Pass this flag to view git assets.',
         )
         parser.add_argument(
-            '-pp',
-            '--personal_pull',
+            '-c',
+            '--clone',
             action='store_true',
             required=False,
             default=False,
-            help='Pull personal repos',
+            help='Pass this flag to clone git assets.',
         )
         parser.add_argument(
-            '-uc',
-            '--users_clone',
+            '-p',
+            '--pull',
             action='store_true',
             required=False,
             default=False,
-            help='Clone user repos.',
+            help='Pass this flag to pull git assets.',
         )
         parser.add_argument(
-            '-up',
-            '--users_pull',
-            action='store_true',
+            '-u',
+            '--users',
+            type=str,
             required=False,
-            default=False,
-            help='Pull user repos.',
+            default=None,
+            help='Pass a comma separated list of users to get repos for.',
         )
         parser.add_argument(
-            '-gc',
-            '--gists_clone',
-            action='store_true',
+            '-o',
+            '--orgs',
+            type=str,
             required=False,
-            default=False,
-            help='Clone personal gists.',
+            default=None,
+            help='Pass a comma separated list of orgs to get repos for.',
         )
         parser.add_argument(
-            '-gp',
-            '--gists_pull',
-            action='store_true',
+            '-g',
+            '--gists',
+            type=str,
             required=False,
-            default=False,
-            help='Pull personal gists.',
+            default=None,
+            help='Pass a comma separated list of users to get gists for.',
         )
         parser.add_argument(
-            '-oc',
-            '--orgs_clone',
-            action='store_true',
-            required=False,
-            default=False,
-            help='Clone organization repos.',
-        )
-        parser.add_argument(
-            '-op',
-            '--orgs_pull',
-            action='store_true',
-            required=False,
-            default=False,
-            help='Pull organization repos.',
-        )
-        parser.add_argument(
-            '-t',
-            '--threads',
-            required=False,
+            '-to',
+            '--timeout',
             type=int,
+            required=False,
+            default=DEFAULT_TIMEOUT,
+            help='The number of seconds before a git operation times out.',
+        )
+        parser.add_argument(
+            '-th',
+            '--threads',
+            type=int,
+            required=False,
             default=DEFAULT_NUM_THREADS,
             help='The number of concurrent threads to run.',
         )
+        parser.add_argument(
+            '-t',
+            '--token',
+            type=str,
+            required=False,
+            default=None,
+            help='Provide your GitHub token to authenticate with the GitHub API and gain access to private repos and gists.',  # noqa
+        )
+        parser.add_argument(
+            '-l',
+            '--location',
+            type=str,  # TODO: Create a custom `path` type here
+            required=False,
+            default=DEFAULT_LOCATION,
+            help='The location where you want your GitHub Archive to be stored.',
+        )
         parser.parse_args(namespace=self)
 
-    def _run(self):
-        GithubArchive.run(
-            personal_clone=self.personal_clone,
-            personal_pull=self.personal_pull,
-            users_clone=self.users_clone,
-            users_pull=self.users_pull,
-            gists_clone=self.gists_clone,
-            gists_pull=self.gists_pull,
-            orgs_clone=self.orgs_clone,
-            orgs_pull=self.orgs_pull,
-            num_of_threads=self.threads,
+    def run(self):
+        github_archive = GithubArchive(
+            view=self.view,
+            clone=self.clone,
+            pull=self.pull,
+            users=self.users,
+            orgs=self.orgs,
+            gists=self.gists,
+            timeout=self.timeout,
+            threads=self.threads,
+            token=self.token,
+            location=self.location,
         )
+        github_archive.run()
 
 
 def main():
-    CLI()._run()
+    GithubArchiveCli().run()
 
 
 if __name__ == '__main__':
