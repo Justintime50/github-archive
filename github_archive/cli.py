ef4785b25c1add88dc8fb364b51ed9f6139998ab
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-09-18 23:27:16 -0600

    chore: prepare project for v4.1.0 release

--- github_archive/cli.py
+++ github_archive/cli.py
@@ -12,36 +12,15 @@ class GithubArchiveCli:
             )
         )
         parser.add_argument(
-            '-v',
-            '--view',
-            action='store_true',
+            '-t',
+            '--token',
+            type=str,
             required=False,
-            default=False,
-            help='Pass this flag to view git assets (dry run).',
-        )
-        parser.add_argument(
-            '-c',
-            '--clone',
-            action='store_true',
-            required=False,
-            default=False,
-            help='Pass this flag to clone git assets.',
-        )
-        parser.add_argument(
-            '-p',
-            '--pull',
-            action='store_true',
-            required=False,
-            default=False,
-            help='Pass this flag to pull git assets.',
-        )
-        parser.add_argument(
-            '-f',
-            '--forks',
-            action='store_true',
-            required=False,
-            default=False,
-            help='Pass this flag to include forked git assets.',
+            default=None,
+            help=(
+                'Provide your GitHub token to authenticate with the GitHub API and gain access to private repos and'
+                ' gists.'
+            ),
         )
         parser.add_argument(
             '-u',
@@ -76,31 +55,36 @@ class GithubArchiveCli:
             help='Pass a comma separated list of users to get starred repos for.',
         )
         parser.add_argument(
-            '-to',
-            '--timeout',
-            type=int,
+            '-v',
+            '--view',
+            action='store_true',
             required=False,
-            default=DEFAULT_TIMEOUT,
-            help='The number of seconds before a git operation times out.',
+            default=False,
+            help='Pass this flag to view git assets (dry run).',
         )
         parser.add_argument(
-            '-th',
-            '--threads',
-            type=int,
+            '-c',
+            '--clone',
+            action='store_true',
             required=False,
-            default=DEFAULT_NUM_THREADS,
-            help='The number of concurrent threads to run.',
+            default=False,
+            help='Pass this flag to clone git assets.',
         )
         parser.add_argument(
-            '-t',
-            '--token',
-            type=str,
+            '-p',
+            '--pull',
+            action='store_true',
             required=False,
-            default=None,
-            help=(
-                'Provide your GitHub token to authenticate with the GitHub API and gain access to private repos and'
-                ' gists.'
-            ),
+            default=False,
+            help='Pass this flag to pull git assets.',
+        )
+        parser.add_argument(
+            '-f',
+            '--forks',
+            action='store_true',
+            required=False,
+            default=False,
+            help='Pass this flag to include forked git assets.',
         )
         parser.add_argument(
             '-l',
@@ -118,23 +102,39 @@ class GithubArchiveCli:
             default=False,
             help='Use HTTPS URLs instead of SSH.',
         )
+        parser.add_argument(
+            '-to',
+            '--timeout',
+            type=int,
+            required=False,
+            default=DEFAULT_TIMEOUT,
+            help='The number of seconds before a git operation times out.',
+        )
+        parser.add_argument(
+            '-th',
+            '--threads',
+            type=int,
+            required=False,
+            default=DEFAULT_NUM_THREADS,
+            help='The number of concurrent threads to run.',
+        )
         parser.parse_args(namespace=self)
 
     def run(self):
         github_archive = GithubArchive(
-            view=self.view,
-            clone=self.clone,
-            pull=self.pull,
-            forks=self.forks,
+            token=self.token,
             users=self.users,
             orgs=self.orgs,
             gists=self.gists,
             stars=self.stars,
-            timeout=self.timeout,
-            threads=self.threads,
-            token=self.token,
+            view=self.view,
+            clone=self.clone,
+            pull=self.pull,
+            forks=self.forks,
             location=self.location,
             use_https=self.https,
+            timeout=self.timeout,
+            threads=self.threads,
         )
         github_archive.run()
 
