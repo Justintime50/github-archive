3c844689b1b265df086b2ad807cba7dc46607662
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-11-24 14:14:37 -0700

    chore: lint and format legacy shell script

--- legacy/github-archive.sh
+++ legacy/github-archive.sh
@@ -3,12 +3,12 @@
 # Import the env file and run some checks
 # shellcheck disable=SC1090,SC2153
 source "${BASH_SOURCE%/*}/.env"
-if [[ -z "$USERNAME" ]] || [[ -z "$TOKEN" ]] || [[ -z "$PAGES" ]] || [[ -z "$DATE" ]] || [[ -z "$LOG_LIFE" ]] || [[ -z "$LOCATION" ]] || [[ -z "$BRANCH" ]] || [[ -z "$USER_ON" ]] || [[ -z "$ORGS_ON" ]] || [[ -z "$GISTS_ON" ]] || [[ -z "$PER_PAGE" ]] || [[ -z "$CLONE_ON" ]] || [[ -z "$PULL_ON" ]] ; then
+if [[ -z "$USERNAME" ]] || [[ -z "$TOKEN" ]] || [[ -z "$PAGES" ]] || [[ -z "$DATE" ]] || [[ -z "$LOG_LIFE" ]] || [[ -z "$LOCATION" ]] || [[ -z "$BRANCH" ]] || [[ -z "$USER_ON" ]] || [[ -z "$ORGS_ON" ]] || [[ -z "$GISTS_ON" ]] || [[ -z "$PER_PAGE" ]] || [[ -z "$CLONE_ON" ]] || [[ -z "$PULL_ON" ]]; then
     # $ORGS is the only optional variable not checked for
     echo "ERROR: You have variables that have no values, please set them in the \".env\" file before continuing."
     exit
 fi
-if [[ "$PER_PAGE" -gt "100" ]] ; then
+if [[ "$PER_PAGE" -gt "100" ]]; then
     echo "ERROR: PER_PAGE cannot be over 100 per Github's API limits. Please correct in your env and run again."
     exit
 fi
@@ -23,26 +23,26 @@ cd "$LOCATION"/repos || exit
 # Start archive
 {
     # Clone all repos from the user's account
-    if [[ "$USER_ON" == "enable" && "$CLONE_ON" == "enable" ]] ; then
+    if [[ "$USER_ON" == "enable" && "$CLONE_ON" == "enable" ]]; then
         echo -e "Cloning personal repos..."
-        for PAGE in ${PAGES[*]} ; do
+        for PAGE in ${PAGES[*]}; do
             mkdir -p "$LOCATION"/repos/"$USERNAME"
             cd "$LOCATION"/repos/"$USERNAME" || exit
             curl -s -H "Authorization: token $TOKEN" "https://api.github.com/user/repos?page=$PAGE&per_page=$PER_PAGE&affiliation=owner" |
-            python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["ssh_url"])'
+                python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["ssh_url"])'
             cd .. || exit
             echo -e "Personal repos cloned!\n"
         done
 
         # Clone all repos from the configured orgs you have access to
-        if [[ "$ORGS_ON" == "enable" ]] ; then
-            for PAGE in ${PAGES[*]} ; do
-                for ORG in ${ORGS[*]} ; do
+        if [[ "$ORGS_ON" == "enable" ]]; then
+            for PAGE in ${PAGES[*]}; do
+                for ORG in ${ORGS[*]}; do
                     echo -e "Cloning $ORG repos..."
                     mkdir -p "$LOCATION"/repos/"$ORG"
                     cd "$LOCATION"/repos/"$ORG" || exit
                     curl -s -H "Authorization: token $TOKEN" "https://api.github.com/orgs/$ORG/repos?page=$PAGE&per_page=$PER_PAGE" |
-                    python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["ssh_url"])'
+                        python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["ssh_url"])'
                     cd .. || exit
                     echo -e "$ORG repos cloned!\n"
                 done
@@ -52,7 +52,7 @@ cd "$LOCATION"/repos || exit
     fi
 
     # Setup dir and message if orgs are enabled
-    if [[ "$ORGS_ON" == "enable" ]] ; then
+    if [[ "$ORGS_ON" == "enable" ]]; then
         DIRARRAY="$USERNAME $ORGS"
         PULLMSG="Personal & organization repos pulled!\n"
     else
@@ -61,12 +61,12 @@ cd "$LOCATION"/repos || exit
     fi
 
     # Pull any changes from each repo in the archive if enabled
-    if [[ "$PULL_ON" == "enable" ]] ; then
+    if [[ "$PULL_ON" == "enable" ]]; then
         echo -e "Pulling existing repos..."
-        for TOPDIR in ${DIRARRAY[*]} ; do
+        for TOPDIR in ${DIRARRAY[*]}; do
             printf '%s\n' "$TOPDIR"
             cd "$TOPDIR" || exit
-            for DIR in */ ; do
+            for DIR in */; do
                 printf '%s\n' "$DIR"
                 cd "$DIR" && git pull origin "$BRANCH"
                 cd .. || exit
@@ -77,19 +77,19 @@ cd "$LOCATION"/repos || exit
     fi
 
     # Clone all gists from the user's account
-    if [[ "$GISTS_ON" == "enable" && "$CLONE_ON" == "enable" ]] ; then
+    if [[ "$GISTS_ON" == "enable" && "$CLONE_ON" == "enable" ]]; then
         echo -e "Cloning personal gists..."
-        for PAGE in ${PAGES[*]} ; do
+        for PAGE in ${PAGES[*]}; do
             cd "$LOCATION"/gists || exit
             curl -s -H "Authorization: token $TOKEN" "https://api.github.com/gists?page=$PAGE&per_page=$PER_PAGE" |
-            python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["html_url"])'
+                python -c $'import json, sys, os\nfor repo in json.load(sys.stdin): os.system("git clone " + repo["html_url"])'
             echo -e "Personal gists cloned!\n"
         done
 
         # Pull any changes from each gist in the archive
-        if [[ "$GISTS_ON" == "enable" && "$PULL_ON" == "enable" ]] ; then
+        if [[ "$GISTS_ON" == "enable" && "$PULL_ON" == "enable" ]]; then
             echo -e "Pulling existing gists..."
-            for DIR in */ ; do
+            for DIR in */; do
                 printf '%s\n' "$DIR"
                 cd "$DIR" && git pull
                 cd .. || exit
